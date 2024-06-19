65d47574e7edd591bc3aa48b4dce541c287a66e9
batmilkyway <116116609+batmilkyway@users.noreply.github.com>
2024-06-19 00:44:46 -0700

    Update python-worker.ts

--- src/workers/python-worker.ts
+++ src/workers/python-worker.ts
@@ -112,7 +112,7 @@ sys.stdin.readline = lambda: react_py.getInput("${id}", __prompt_str__)
     const result = await self.pyodide.runPythonAsync(code)
     return result
   },
-  async runSync(code: string): any {
+  async runSync(code: string): Promise<any> {
     const result = self.pyodide.runPython(code)
     return result
   },
